af311696a5db2fc57f1158891e9d9d2d5237c3ae
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-10-29 22:41:21 +0100

    about

---
+++ pages/about.js
@@ -0,0 +1,98 @@
+import {
+    connect,
+    MyHead as Head,
+    Wrapper,
+    reduxWrapper,
+    configure,
+    resizeCloudinaryImage,
+    MyTypography
+  } from 'eventjuicer-site-components';
+ 
+
+  import settings from '../settings';
+  
+  const PageIndex = (props) => (
+
+    <div>
+  
+
+    
+    <Wrapper first>
+
+    <MyTypography template="h4">
+    In 2021 the E-commerce Berlin Expo will take place for the 6th time. When we chose to expand the proven Expo formula back in 2018, we decided to add the Networking Party a day before the actual event. 
+    </MyTypography>
+
+    <MyTypography template="subtitle1" >
+    We immediately knew that an additional evening spent with the industry leaders would be highly valuable for all participants. Since then, it has become an awaiting tradition for e-commerce professionals all over Europe, and over 2000 guests had a chance to celebrate such an evening with us. 
+    </MyTypography>
+
+
+    <MyTypography template="subtitle1" >
+    The Party consists of two parts: a pure Networking part and the E-commerce Germany Awards Ceremony.
+    </MyTypography>
+ 
+    </Wrapper>
+
+
+
+
+<Wrapper>
+    {[
+      {
+        image:
+          'https://res.cloudinary.com/eventjuicer/image/upload/v1604003805/ega/ega_2021_numberOfContestVoters.svg',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/eventjuicer/image/upload/v1604003805/ega/ega_2021_awardsCeremonyGuests.svg',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/eventjuicer/image/upload/v1604003805/ega/ega_2021_numberOfContestParticipants.svg',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/eventjuicer/image/upload/v1604003805/ega/ega_2021_promotion.svg',
+      },
+     
+    ].map(item => (
+      <img
+        key={item.image}
+        src={resizeCloudinaryImage(item.image, 1500, 1000)}
+        style={{
+          width: '100%',
+          margin: '0 auto',
+        }}
+      />
+    ))}
+  </Wrapper>
+
+  </div>
+  
+    
+  )
+   
+  
+ 
+  export const getStaticProps = reduxWrapper.getStaticProps(async ({ store }) => {
+  
+    await configure(store, {
+      settings: settings,
+      preload: ['contestant_companies_all']
+    })
+  
+    return {
+      props: {},
+      revalidate: 1
+    }
+    
+  })
+  
+  
+  
+  export default connect()(PageIndex);
+  
+  
+  
+  
