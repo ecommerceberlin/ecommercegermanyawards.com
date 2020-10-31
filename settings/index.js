30a0cafa98bdeb207d1fe4a5c52cfb4a2bb40bb1
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-10-31 01:47:45 +0100

    change initial fields

--- settings/index.js
+++ settings/index.js
@@ -211,7 +211,7 @@ const settings  = {
               {name: "accept", required: true, type: "confirm"}
             ],
 
-            start : ['email', 'fname'],
+            start : ['cname2', 'project_name', 'awards_category'],
             ticket_id : 1830,
             email_template : "ecommerceberlin-visitor-registration",
             background : "https://res.cloudinary.com/ecommerceberlin/image/upload/c_fit,h_500,w_500/v1546813408/ebe_lanyard1.jpg",
