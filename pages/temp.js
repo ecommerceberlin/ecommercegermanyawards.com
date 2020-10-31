b47e5f9c95836dca07078ac25f5611e42f3bb865
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-10-31 01:17:19 +0100

    2.6.0

--- pages/temp.js
+++ pages/temp.js
@@ -28,22 +28,17 @@ const PageIndex = (props) => (
 
   <div>
 
-
-
   <Wrapper first label="awards.hello.title">
   
   <Section 
    
-   left={  <div style={{marginTop: '5rem'}}>
-
-     <MyTypography template="h4" label="awards.hello.submit" />
-    
-     <MyTypography template="subtitle1" label="awards.hello.needs" />
-
-     <Markdown label="awards.hello.details" />
-
-
-     </div> }
+   left={  
+      <div style={{marginTop: '5rem'}}>
+      <MyTypography template="h4" label="awards.hello.submit" />
+      <MyTypography template="subtitle1" label="awards.hello.needs" />
+      <Markdown label="awards.hello.details" />
+      </div> 
+    }
    right={ 
      <WidgetVerticalTimeline 
      setting="awardstimeline" 
@@ -55,10 +50,8 @@ const PageIndex = (props) => (
        Assessment: <Icons.Assessment />
      }} />
    }
-
  leftCentered={true}
-
- />
+/>
 
 
  </Wrapper>
@@ -81,123 +74,69 @@ const PageIndex = (props) => (
     'innovation'
   ]
   }} 
-
   right={
     <>
     <MyTypography template="subtitle1" label="awards.rules-summary.title" /> 
     <Markdown label="awards.rules-summary.body" />
     </>
   }
-
  summary={<div>asd</div>}
-
  />
 
-
-<WidgetIconGrid setting="contestant.categories" icons={{
-
-}}/>
-
-
-
-   <WidgetFaq setting="contestant.faq" />
-
-
+<WidgetIconGrid setting="contestant.categories" icons={{}}/>
+<WidgetFaq setting="contestant.faq" />
 <WidgetPhotostream setting="awardsphotostream" />
+<WidgetJurors
+  label="awards.jury.title"
+  secondaryLabel="awards.jury.description"
+  disableTemps={false}
+  limit={100}
+  filter={null}
+  bio={false}
+  minToShow={1}
+/>
 
+<WidgetContestantCompaniesArchiveWinners />
 
-   {/* <div>
+<WidgetPartners
+label="partners.networking.title"
+filter={item =>
+item['scopes(deprecated)'].indexOf('networking') > -1 &&
+item.logotype.indexOf('cloudinary') > -1
+}
+limit={50}
+center={true}
+/>
 
-   Why speak at the E-commerce Berlin Expo?
-Over 7,000 e-commerce professionals
-90% average session capacity
-3,000+ CEOs, Directors & Heads of Departments
-Relevant visitors, 65% of whom come from retailers & brands
+<WidgetPartners
+label="partners.media.title"
+filter={item =>
+item['scopes(deprecated)'].indexOf('media') > -1 &&
+item.logotype.indexOf('cloudinary') > -1
+}
+limit={50}
+/>
 
+<WidgetPartners
+label="partners.community.title"
+filter={item =>
+item['scopes(deprecated)'].indexOf('community') > -1 &&
+item.logotype.indexOf('cloudinary') > -1
+}
+limit={50}
+/>
 
-   </div> */}
+<WidgetPartners
+label="partners.communication.title"
+filter={item =>
+item['scopes(deprecated)'].indexOf('communication') > -1 &&
+item.logotype.indexOf('cloudinary') > -1
+}
+center={true}
+limit={50}
+/>
 
-   {/*
-
-Website Conversion Rate
-Marketing in E-commerce
-eLogistics / International Expansion
-ePayments
-IT for E-commerce
-New Developments in E-commerce
-
-*/}
-
- 
-   
-
-   <WidgetJurors
-          label="awards.jury.title"
-          secondaryLabel="awards.jury.description"
-          disableTemps={false}
-          limit={100}
-          filter={null}
-          bio={false}
-          minToShow={1}
-        />
-
-
-
-
-   {/* <WidgetPresentersAll 
-     filter={(item)=> parseInt(item.featured_cfp) } limit="24" 
-     label="cfp.featured_presenters"
-   /> */}
-
-      {/* <WidgetContestantCompanies label="awards.contestants.categories.title" /> */}
-
-
-     <WidgetContestantCompaniesArchiveWinners />
-
-
-     <WidgetPartners
-          label="partners.networking.title"
-          filter={item =>
-            item['scopes(deprecated)'].indexOf('networking') > -1 &&
-            item.logotype.indexOf('cloudinary') > -1
-          }
-          limit={50}
-          center={true}
-        />
-
-        <WidgetPartners
-          label="partners.media.title"
-          filter={item =>
-            item['scopes(deprecated)'].indexOf('media') > -1 &&
-            item.logotype.indexOf('cloudinary') > -1
-          }
-          limit={50}
-        />
-
-        <WidgetPartners
-          label="partners.community.title"
-          filter={item =>
-            item['scopes(deprecated)'].indexOf('community') > -1 &&
-            item.logotype.indexOf('cloudinary') > -1
-          }
-          limit={50}
-        />
-
-        <WidgetPartners
-          label="partners.communication.title"
-          filter={item =>
-            item['scopes(deprecated)'].indexOf('communication') > -1 &&
-            item.logotype.indexOf('cloudinary') > -1
-          }
-          center={true}
-          limit={50}
-        />
-
-
-
-            <WidgetVideoWithEventInfo />
-
-
+<WidgetVideoWithEventInfo />
 
 </div>
 
