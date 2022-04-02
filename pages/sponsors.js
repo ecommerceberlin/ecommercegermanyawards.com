ed15c1f82506bd84bdccf1d7227d8937cc8a8d4c
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-02 13:48:37 +0200

    added sponsors

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -14,7 +14,9 @@ const PageSponsors = (props) => (
     <div>
     <Wrapper first label="awards.sponsors.list.title">
         <CompanyCardSimple id={2028} label="awards.sponsor.headline" labelParam="2022" />
-        <CompanyCardSimple id={1939} label="awards.sponsor.year" labelParam="2022" />
+        <CompanyCardSimple id={1939} label="awards.sponsor.drinkables" labelParam="2022" />
+        <CompanyCardSimple id={2044} label="awards.sponsor.supporting" labelParam="2022" />
+
         <CompanyCardSimple id={1822} label="awards.sponsor.year" labelParam="2021" />
         <CompanyCardSimple id={1061} label="awards.sponsor.year" labelParam="2020"/>
         <CompanyCardSimple id={1366} label="awards.sponsor.year" labelParam="2019"/>
@@ -28,6 +30,8 @@ export const getStaticProps = reduxWrapper.getStaticProps(async ({ store }) => {
   await configure(store, {
     settings: settings,
     preload: [
+        'companies/2044', //mail alliance
+        'companies/2028', //trevipay
         'companies/1939', //emplifi.io
         'companies/1822', //hypernode
         'companies/1061', //leaseweb
