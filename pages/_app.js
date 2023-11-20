9e55d7cd77fd0c646d83182cd1c770332863e3e9
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2023-11-20 16:28:59 +0100

    EGA touch-ups

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { reduxWrapper, NextApp, MyHead } from 'eventjuicer-site-components';
 import Head from 'next/head'
 
 const MyCustomApp = (props) => <NextApp {...props} head={
-    <MyHead image="https://res.cloudinary.com/eventjuicer/image/upload/w_1500,h_1000,c_fit/v1604012184/ega/award1.jpg" titleLabel="awards.opengraph.name" descriptionLabel="awards.opengraph.description" url="/">{(data) => <Head>{data}</Head>}</MyHead> 
+    <MyHead image="https://res.cloudinary.com/eventjuicer/image/upload/w_1500,h_1000,c_fit/v1700489119/IMG_7486.jpg" titleLabel="awards.opengraph.name" descriptionLabel="awards.opengraph.description" url="/">{(data) => <Head>{data}</Head>}</MyHead> 
 } />
 
 export default reduxWrapper.withRedux(MyCustomApp)
